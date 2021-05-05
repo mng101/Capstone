1f295c4092ff7415060cb8a6b5c5150bfb284e6b
mng101 <mahesh.gidwani@sympatico.ca>
2021-05-04 21:14:07 -0400

    Completed Index view of "Markets Today"

--- challenge/views.py
+++ challenge/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils import timezone
+from django.template import Library
 
 from django.views.generic.base import TemplateView
 from django.contrib import messages
@@ -8,8 +9,10 @@ from django.views.generic import (View, TemplateView, ListView, DetailView,
                                   CreateView, DeleteView, UpdateView, )
 import requests
 import json
+import numpy
 
 from . import forms
+
 # from .models import Account
 
 # Create your views here.
@@ -17,6 +20,8 @@ from . import forms
 # Define the interval during which repeated API calls for market data are to be avoided
 #
 MARKET_DATA_REFRESH_INTERVAL = 3600
+register = Library()
+
 
 class HomePageView(TemplateView):
     template_name = "challenge/home.html"
@@ -55,12 +60,30 @@ def get_market_summary(request):
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    if (response.status_code == 200):
+    if response.status_code == 200:
         # Deserialize the response to a python object
         json_data = json.loads(response.text)
         return json_data
     else:
         return None
+
+
+# def split_into_columns(indices, n):
+#     # Split the list into n columns to display as a table
+#     # Adapted from //djangosnippets.org/snippets/401
+#     try:
+#         items = list(indices)
+#         columns = int(n)
+#     except (ValueError, TypeError):
+#         return [indices]
+#
+#     list_len = len(items)
+#     rows = list_len // columns
+#
+#     if list_len % n != 0:
+#         rows += 1
+#
+#     return [items[split * i:split * (i + 1):split * (i + 2)] for i in range(n)]
 
 
 def refresh_market_summary(request):
@@ -84,28 +107,11 @@ def refresh_market_summary(request):
         request.session['indices'].append(get_market_summary(request)['marketSummaryAndSparkResponse'])
         request.session["indices"][0]["timestamp"] = now
 
-    return request.session['indices'][1]["result"]
-
-
-    #
-    # if "api_call_timestamp" not in request.session:
-    #     request.session["api_call_timestamp"] = 0
-    #
-    # now = timezone.now().timestamp()
-    #
-    # if (now - request.session['api_call_timestamp']) > MARKET_DATA_REFRESH_INTERVAL:
-    #     # Exceeded the refresh interval. Refresh data
-    #     request.session['indices'] = get_market_summary(request)['marketSummaryAndSparkResponse']
-    #
-    #     # json_data is returned as an array of 15 elements. We will return the complete array for addition
-    #     # into the "context". The elements to be display will be determined in the template
-    #
-    # return 0
+    return numpy.array_split(request.session['indices'][1]["result"], 5)
 
 
 class MarketsPageView(TemplateView):
     template_name = 'challenge/markets.html'
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -113,23 +119,6 @@ class MarketsPageView(TemplateView):
         # Add list of market indices and values
         context["index_list"] = refresh_market_summary(self.request)
 
-        # Create "indices" and "headlines" keys in the User session
-        # The values retreived from the Finance API will be cached in the User session for
-        # subsequent visits to the same page to limit the number of API calls during a User session.
-        # The timestamp for the last API call is also saved, which is used to determine
-        # when the data should be refreshed from the source
-
-        # if "indices" not in self.request.session:
-        #     self.request.session["indices"] = []
-        # if "headlines" not in self.request.session:
-        #     self.request.session["headlines"] = []
-        # if "api_call_timestamp" not in self.request.session:
-        #     self.request.session["api_call_timestamp"] = 0
-
-        # Refresh Market Data
-        # refresh_market_data(self.request)
-
-        messages.info(self.request, "hello http://example.com")
         return context
 
 # TODO - Compete the AccountUpdateView
